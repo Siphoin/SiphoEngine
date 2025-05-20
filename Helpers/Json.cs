ad69c6dd2007143955fe7e2d29a952eaff089fbb
Maxim <36982547+Siphoin@users.noreply.github.com>
2025-05-20 17:48:21 +0500

    Create Json.cs

---
+++ Helpers/Json.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+
+namespace SiphoEngine.Helpers
+{
+    public static class Json
+    {
+        public static string ToJson(object? value, JsonSerializerSettings settings = default, Formatting formatting = Formatting.None)
+        {
+            if (settings is null)
+            {
+                settings = new JsonSerializerSettings();
+            }
+            return JsonConvert.SerializeObject(value, formatting, settings: settings);
+        }
+
+        public static T FromJson<T>(string value, JsonSerializerSettings? settings)
+        {
+            return JsonConvert.DeserializeObject<T>(value, settings);
+        }
+    }
+}
